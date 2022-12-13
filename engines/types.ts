0d4bd05371320582f14154b90513ad54cd1fedf7
alexdanilowicz <alex.danilowicz@gmail.com>
2022-12-12 18:43:10 -0800

    AlexBot

--- engines/types.ts
+++ engines/types.ts
@@ -1,5 +1,6 @@
 import { Move } from 'chess.js'
 
+import { AlexBot } from './AlexBot'
 import { BasicMinimaxD23S } from './BasicMinimax'
 import { RandomBot } from './RandomBot'
 import { TeddyBot } from './TeddyBot'
@@ -10,4 +11,4 @@ export type EngineResponse = {
   numberOfNodesSearched: number
 }
 
-export const Engines = [RandomBot, BasicMinimaxD23S, TeddyBot]
+export const Engines = [RandomBot, BasicMinimaxD23S, TeddyBot, AlexBot]
